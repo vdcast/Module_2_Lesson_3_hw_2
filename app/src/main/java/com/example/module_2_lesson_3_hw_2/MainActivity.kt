db195c8611491b47dc434c55464aa510f359032f
VD <vdfree448@protonmail.com>
2022-07-02 15:29:42 +0300

    added Constraint layouts: clInfo, clPin basic logic to gone/visible these layouts

--- app/src/main/java/com/example/module_2_lesson_3_hw_2/MainActivity.kt
+++ app/src/main/java/com/example/module_2_lesson_3_hw_2/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.module_2_lesson_3_hw_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        clInfo.visibility = View.GONE
+
+        val pin = "0000"
+
+        btPin.setOnClickListener {
+            if (etPin.text.toString() == pin){
+                clInfo.visibility = View.VISIBLE
+                clPin.visibility = View.GONE
+            }else{
+                tvAlert.text = "Wrong PIN. Try again."
+            }
+        }
+
+        btCheckBalance.setOnClickListener {
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
