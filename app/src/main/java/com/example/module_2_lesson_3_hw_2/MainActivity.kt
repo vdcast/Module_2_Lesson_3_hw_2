3b37ccb3c7ac8dea056c5a0f7ff47b37f542c1e0
VD <vdfree448@protonmail.com>
2022-07-02 16:04:09 +0300

    added logic to clear editText with PIN and to GONE for clInfo and VISIBLE for clPin

--- app/src/main/java/com/example/module_2_lesson_3_hw_2/MainActivity.kt
+++ app/src/main/java/com/example/module_2_lesson_3_hw_2/MainActivity.kt
@@ -3,19 +3,20 @@ package com.example.module_2_lesson_3_hw_2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val pin = "0000"
 
         clInfo.visibility = View.GONE
-
-        val pin = "0000"
 
         btPin.setOnClickListener {
             if (etPin.text.toString() == pin){
@@ -32,4 +33,13 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onResume() {
+        super.onResume()
+        clInfo.visibility = View.GONE
+        clPin.visibility = View.VISIBLE
+        etPin.text.clear()
+        tvAlert.text = ""
+    }
+
 }
